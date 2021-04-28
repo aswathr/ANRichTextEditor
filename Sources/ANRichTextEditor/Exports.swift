c29f40aa7158b7b75be2c142e489b0d1369b1469
Aswath <aswathrce@gmail.com>
2021-04-28 19:14:11 +0530

    Added SwiftPM support

---
+++ Sources/ANRichTextEditor/Exports.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Aswath Narayanan on 28/04/21.
+//
+
+@_exported import ANRichTextEditor_ObjC
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import ANRichTextEditorTests
+
+var tests = [XCTestCaseEntry]()
+tests += ANRichTextEditorTests.allTests()
+XCTMain(tests)
